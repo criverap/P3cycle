e5a229fff617f18ea7551f9dc8076a75e04b48bd
Alfredo Segundo <asegundo@thoughtworks.com>
2017-12-19 01:53:53 -0300

    Disabling service worker until needed.

--- src/src/index.js
+++ src/src/index.js
@@ -5,4 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//Disabling ServiceWorker while we don't have an production deploy.
+//registerServiceWorker();
